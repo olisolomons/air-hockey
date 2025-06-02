c53cc6d39f5182d98174146537311ca2dcbc457a
olisolomons <oli.solomons@gmail.com>
2025-06-02 14:06:08 +0000

    bounce

--- src/air_hockey/game.cljs
+++ src/air_hockey/game.cljs
@@ -3,6 +3,13 @@
    [air-hockey.sketch-functions :as sf]
    [quil.core :as q]
    [threejs-math :as t]))
+
+(extend-protocol IPrintWithWriter
+  t/Vector2
+  (-pr-writer [obj writer opts]
+    (doseq [part ["#vec [" (.-x obj) " " (.-y obj) "]"]]
+      (-write writer part))))
+
 (defn ->vec [x y]
   (t/Vector2. x y))
 (defn <-vec [v]
@@ -16,10 +23,25 @@
 (defn v* [^t/Vector p1 coefficient]
   (.multiplyScalar ^t/Vector (.clone p1) coefficient))
 (defn ->line [^t/Vector p1 ^t/Vector p2]
-  {:p p1
-   :dir (v- p2 p1)})
+  (let [dir (v- p2 p1)]
+    {:p1 p1
+     :p2 p2
+     :dir dir
+     :normal (-> dir
+                 .clone
+                 (.rotateAround
+                  (->vec 0 0)
+                  (/ js/Math.PI 2))
+                 .normalize)}))
+
+(defn- point-on-line [l2 t2]
+  (-> l2
+      :dir
+      (v* t2)
+      (v+ (:p1 l2))))
+
 (defn intersection [l1 l2]
-  (let [pos-diff (v- (:p l1) (:p l2))
+  (let [pos-diff (v- (:p1 l1) (:p2 l2))
         m (t/Matrix3.)
         _ (.set m
                 (.-x (:dir l1))
@@ -28,18 +50,51 @@
                 (.-y (:dir l1))
                 (.-y (:dir l2))
                 0
-                0 0 1) ]
+                0 0 1)]
     (if (zero? (.determinant m))
       nil
-      (do (.invert m)
-          (.applyMatrix3 pos-diff m)
-          {:t1 (- (.-x pos-diff))
-           :t2 (.-y pos-diff)
-           :p (v+ (v* (:dir l2) (.-y pos-diff)) (:p l2))}))))
+      (let [_ (.invert m)
+            _ (.applyMatrix3 pos-diff m)
+            t1 (- (.-x pos-diff))
+            t2 (- (.-y pos-diff))]
+        (def t1 t1)
+        (def t2 t2)
+        (when (and (< 0 t1 1) (< 0 t2 1))
+          {:t1 t1
+           :t2 t2
+           :l1 l1
+           :l2 l2})))))
+(defn not-zero [number]
+  (when-not (zero? number) number))
+
+(defn intersection-offset [offset l1 l2]
+  (when-let [qwe (not-zero ^js/Number (.dot (:dir l1) (:normal l2)))]
+    (let [t-offset (js/Math.abs (/ ^js/Number offset ^js/Number qwe))
+          pos-diff (v- (:p1 l1) (:p2 l2))
+          m (t/Matrix3.)
+          _ (.set m
+                  (.-x (:dir l1))
+                  (.-x (:dir l2))
+                  0
+                  (.-y (:dir l1))
+                  (.-y (:dir l2))
+                  0
+                  0 0 1)
+          _ (.invert m)
+          _ (.applyMatrix3 pos-diff m)
+          t1 (- (- (.-x pos-diff))
+                t-offset)
+          t2 (- (.-y pos-diff))]
+      (when (and (< 0 t1 1) (< 0 t2 1))
+        {:t1 t1
+         :t2 t2
+         :l1 l1
+         :l2 l2}))))
 (comment
   (def l1 (->line (->vec 0 0) (->vec 2 2)))
   (def l2 (->line (->vec 2 0) (->vec 0 2)))
-  (<-vec (:p (intersection l1 l2)))
+  (intersection l1 l2)
+  (point-on-line l2 0.5)
   (-> (->vec 5 3)
       (v* 2)
       <-vec)
@@ -53,21 +108,65 @@
   (def a (js/Array.))
   (.-elements m))
 
-(defn mk-state [size]
+(defn mk-state [[width height :as size]]
   (sf/add-derived-state
    {:canvas-size size
-    :puck {:p1 {:pos [50 50]}
-           :p2 {:pos [50 (- (second size) 50)]}}
+    :puck {:p1 {:pos (->vec 50 50)}
+           :p2 {:pos (->vec 50 (- height 50))}}
+    :lines [(->line (->vec 0 0) (->vec 0 height))
+            (->line (->vec 0 0) (->vec width 0))
+            (->line (->vec 0 height) (->vec width height))
+            (->line (->vec width 0) (->vec width height))]
+
     :type :game}))
 
-(defn- move-puck [puck ds]
-  (assoc puck :pos (map + (:pos puck) ds)))
+(defn reflect [v ^t/Vector2 wall-normal]
+  (let [d (.dot wall-normal v)]
+    (v+ v (v* wall-normal (* d -2)))))
+
+(extend-protocol IPrintWithWriter
+  t/Vector2
+  (-pr-writer [obj writer opts]
+    (doseq [part ["#vec [" (.-x obj) " " (.-y obj) "]"]]
+      (-write writer part))))
+
+(def puck-radius 20)
+
+(defn- move-puck [puck ds lines]
+  (def puck puck)
+  (def ds ds)
+  (def lines lines)
+  (let [pos (:pos puck)
+        velocity (:velocity puck)
+        move {:p1 pos
+              :dir ds}
+        first-hit (->> lines
+                       (keep (partial intersection-offset puck-radius move))
+                       (reduce
+                        (fn [a b]
+                          (if (< (:t1 b) (:t1 a))
+                            b a))))
+
+        _ (def move move)
+        _ (def first-hit first-hit)
+        _ (comment
+            (point-on-line (first lines) (- 1 (:t2 first-hit))))
+        new-pos (point-on-line move (or (:t1 first-hit) 1))
+        puck (assoc puck :pos new-pos)]
+    (if first-hit
+      (let [puck (assoc puck :velocity
+                        (reflect velocity (:normal (:l2 first-hit))))
+            new-ds (v* (reflect ds (:normal (:l2 first-hit))) (- 1 (:t1 first-hit)))]
+        (move-puck puck
+                   (v* new-ds (- 1 (:t1 first-hit))) lines))
+      puck)))
+
 (defn- update-state [state dt]
   (-> state
       (update :puck update-vals
               (fn [{:keys [velocity] :as puck}]
                 (cond-> puck
-                  velocity (move-puck (map (partial * dt) velocity)))))))
+                  velocity (move-puck (v* velocity dt) (:lines state)))))))
 
 (defmethod sf/update-state :game
   [{:keys [previous-time] :as state}]
@@ -79,38 +178,38 @@
        state)
      :previous-time now)))
 
-(def puck-radius 20)
-
 (defn draw-circle [x y radius]
   (q/ellipse x y (* 2 radius) (* 2 radius)))
-
 (defmethod sf/draw-state :game
-  [{:keys [puck] [width height] :canvas-size}]
+  [{:keys [puck lines] [width height] :canvas-size}]
+  (q/no-stroke)
   (q/fill 50 0 0)
   (q/rect 0 0 width (/ height 2))
   (q/fill 255 50 50)
   (q/rect 0 (/ height 2) width 0)
-  (doseq [[_player {[x y] :pos}] puck]
-    (draw-circle x y puck-radius)))
+  (doseq [[_player {:keys [pos]}] puck
+          :let [[x y] (<-vec pos)]]
+    (draw-circle x y puck-radius))
+  (q/stroke 255 255 255)
+  (doseq [{:keys [p1 p2]} lines]
+    (def p1 p1)
+    (q/line (<-vec p1) (<-vec p2))))
 
 (defmethod sf/add-derived-state :game
   [state]
   state)
-
-(defn in-dist? [p1 p2 limit]
-  (let [[dx dy] (map - p2 p1)]
-    (< (+ (* dx dx) (* dy dy))
-       (* limit limit))))
 
 (defmethod sf/touch-start :game
   [state {:keys [x y id]}]
   (if-let [player
            (some
             (fn [[player {:keys [pos]}]]
-              (when (in-dist? pos [x y] puck-radius)
+              (when (< (.distanceTo ^t/Vector2 pos (->vec x y)) puck-radius)
                 player))
             (:puck state))]
-    (assoc-in state [:dragging id] {:player player :history #queue []})
+    (-> state
+        (assoc-in [:dragging id] {:player player :history #queue []})
+        (update-in [:puck player] dissoc :velocity))
     state))
 
 (defn- conj-limit [queue element limit]
@@ -121,10 +220,17 @@
 (defmethod sf/touch-move :game
   [{:keys [dragging] :as state} {:keys [id x y]}]
   (if-let [{:keys [player]} (get dragging id)]
-    (-> state
-        (assoc-in [:puck player :pos] [x y])
-        (update-in [:dragging id :history]
-                   conj-limit [x y (q/millis)] 20))
+    (do
+      (def state state)
+      (def player player)
+      (def x x)
+      (def y y)
+      (def id id)
+      (def ms (q/millis))
+      (-> state
+          (assoc-in [:puck player :pos] (->vec x y))
+          (update-in [:dragging id :history]
+                     conj-limit [x y (q/millis)] 5)))
     state))
 
 (defmethod sf/touch-end :game
@@ -137,6 +243,8 @@
                      (let [[x y t] (peek history)
                            dt (- (q/millis) t)]
                        (assoc puck :velocity
-                              (map #(/ % dt)
-                                   (map - (:pos puck) [x y])))))))
+                              (-> puck
+                                  :pos
+                                  (v- (->vec x y))
+                                  (vdiv dt)))))))
     state))
